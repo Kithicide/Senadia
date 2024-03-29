08e5b4179801451ffc7b41fb6c71e0a6176cfc1b
Kithicide <165344707+Kithicide@users.noreply.github.com>
2024-03-28 21:32:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Finding Cays Beta Crystals","I am Best Girl"];
 
 
 let currentIndex = 0;
